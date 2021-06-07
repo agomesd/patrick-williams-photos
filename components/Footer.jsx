d8d31e6caee63adeea45af39bd4c29375e9a64d9
Alex Gomes <alex.gduguay@gmail.com>
2021-06-07 20:54:33 +0200

    Add footer

---
+++ components/Footer.jsx
@@ -0,0 +1,49 @@
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <p>© 2021 all rights reserved</p>
+        <a
+          className={styles.link}
+          href="https://murmuring-peak-27984.herokuapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Created by Gomie Web Designs
+        </a>
+      </div>
+      <p className={styles.top} onClick={handleScrollToTop}>
+        Back to Top
+      </p>
+      <div className={styles.iconBox}>
+        <a
+          href="https://www.instagram.com/patrickwilliamsphotos/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon fontSize="large" className={styles.icon} />
+        </a>
+        <a
+          href="https://www.facebook.com/patrick.p.williams.37"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FacebookIcon fontSize="large" className={styles.icon} />
+        </a>
+        <MailOutlineIcon fontSize="large" className={styles.icon} />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import BlogCard from "../components/BlogCard";
 import ImageCard from "../components/ImageCard";
 import styles from "../styles/Home.module.css";
+import Footer from "../components/Footer";
 
 const Index = () => {
   const [loading, setLoading] = useState(false);
@@ -38,7 +39,9 @@ const Index = () => {
       <div className={styles.about}>
         <div className={styles.backImage}></div>
         <div className={styles.content}>
-          <h2 className={`${styles.title} ${styles.heading}`}>Patrick Williams Photography</h2>
+          <h2 className={`${styles.title} ${styles.heading}`}>
+            Patrick Williams Photography
+          </h2>
           <p className={styles.description}>
             Bacon ipsum dolor amet turducken corned beef cupim, boudin
             prosciutto spare ribs pork andouille landjaeger ribeye.
@@ -54,23 +57,22 @@ const Index = () => {
           <BlogCard data={blogs[1]} loading={loading} />
           <BlogCard data={blogs[2]} loading={loading} />
         </div>
-        <Link href='/blogs'>
+        <Link href="/blogs">
           <a className={`${styles.blogLink} ${styles.link}`}>All Blog Posts</a>
         </Link>
       </div>
       <div className={styles.portfolio}>
-        <h1 className={styles.title}>
-          Portfolio
-        </h1>
+        <h1 className={styles.title}>Portfolio</h1>
         <div className={styles.slideshow}>
-          <ImageCard data={blogs[0]}/>
-          <ImageCard data={blogs[1]}/>
-          <ImageCard data={blogs[2]}/>
+          <ImageCard data={blogs[0]} />
+          <ImageCard data={blogs[1]} />
+          <ImageCard data={blogs[2]} />
         </div>
-        <Link href='/portfolio'>
+        <Link href="/portfolio">
           <a className={`${styles.blogLink} ${styles.link}`}>Portfolio</a>
         </Link>
       </div>
+      <Footer />
     </div>
   );
 };
